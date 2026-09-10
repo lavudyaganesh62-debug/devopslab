a3afc87cbf4f232a38888fb4f4bd3fd535e01d7e
lavudyaganesh62-debug <lavudyaganesh62@gmail.com>
2026-09-10 22:22:45 +0530

    Clean up app.py and resolve merge conflicts
    Removed merge conflict markers and cleaned up print statements.

--- app.py
+++ app.py
@@ -1,18 +1,2 @@
 print("this is from ganesh")
-hii
-hlooo
-<<<<<<< HEAD
-bye
-<<<<<<< HEAD
-ganesh
-=======
-ganesh
-iam coming
-
-=======
-hii ganesh
-<<<<<<< HEAD
->>>>>>> b94ece1ad734e295996164f8ecafdc0301856fc8
-=======
-print("hi ganesh")
->>>>>>> b73030a01b1d6b307d242edb9a25848379788a26
+print("hii ganesha")
